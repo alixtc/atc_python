7c05fdf046dc5c90bd476b177f11e98f56adc6b8
Alix Tiran-Cappello <alix.tiran-cappello@laposte.net>
2021-10-30 10:54:49 +0200

    Wrapped in Class and add mapping dict

--- atc_python/clean_names.py
+++ atc_python/clean_names.py
@@ -1,28 +1,42 @@
 import unicodedata
 import pandas as pd
 
-def strip_accents(text):
-    return ''.join(c for c in unicodedata.normalize('NFKD', text)
-                   if unicodedata.category(c) != 'Mn')
+class CleanNames:
+    def __init__(self, dataframe: pd.DataFrame):
+        self.names = self.clean_names(dataframe)
+        self.new_old_match = self.create_matching_dict(dataframe)
 
-def clean_one_name(name: str) -> str:
-    new_name = ""
-    for s in name:
-        if (s.isalpha() or s.isdigit() or s.isspace()):
-            new_name += s
-        else:
-            new_name += " "
+    def strip_accents(self, text):
+        return ''.join(c for c in unicodedata.normalize('NFKD', text)
+                    if unicodedata.category(c) != 'Mn')
 
-    new_name = strip_accents(new_name).lower()
-    new_name = new_name.replace("  ", " ").strip()
-    new_name = new_name.replace(" ", "_")
-    return new_name
+    def clean_one_name(self, name: str) -> str:
+        """
+        Remove accents, strips terminal and double white spaces,
+        change spaces to underscores, and lower case everywhere
+        """
+        new_name = ""
+        for s in name:
+            if (s.isalpha() or s.isdigit() or s.isspace()):
+                new_name += s
+            else:
+                new_name += " "
+        new_name = self.strip_accents(new_name).lower()
+        new_name = new_name.replace("  ", " ").strip()
+        new_name = new_name.replace(" ", "_")
+        return new_name
 
-def clean_names(dataframe: pd.DataFrame) -> list:
-    """
-    Returns the columns names of a dataframe with only latin letters
-    written in snake_case
-    """
-    column_names = dataframe.columns
-    new_names = [clean_one_name(name) for name in column_names]
-    return new_names
+    def clean_names(self, dataframe: pd.DataFrame) -> list:
+        """
+        Returns the columns names of a dataframe with only latin letters
+        written in snake_case
+        """
+        column_names = dataframe.columns
+        new_names = [self.clean_one_name(name) for name in column_names]
+        return new_names
+
+    def create_matching_dict(self, dataframe: pd.DataFrame) -> dict:
+        old_names = dataframe.columns
+        matching_dict = {new: old for new, old in zip(self.names, old_names)}
+        self.old_new_match = matching_dict
+        return self
--- tests/test_clean_names.py
+++ tests/test_clean_names.py
@@ -1,29 +1,38 @@
-from atc_python.clean_names import clean_names, clean_one_name
+from atc_python.clean_names import CleanNames
 import pandas as pd
 import numpy as np
 import re
 
-def test_clean_one_name():
-    new_name = clean_one_name('T° tunnel gélification op1260(10st4)')
-    assert new_name == 't_tunnel_gelification_op1260_10st4'
-
-
 COLUMN_NAMES = [
-    'Carton Rouge_op1260', 'température four cuissonop1260',
-    'T° tunnel gélification op1260(10st4)', 'T° tunnel pré-chauffage op1260',
-    'température tunnel de maintien op1260', 'T° réservoir résine op1260',
-    '1260_T° Lampe IR 1', '1260_T° Lampe IR 2', '1260_T° Lampe IR 3',
-    'Poids résine piece sortie op1260', 'N° Série',
+    'Carton Rouge_op1260',
+    'température four cuissonop1260',
+    'T° tunnel gélification op1260(10st4)',
+    'T° tunnel pré-chauffage op1260',
+    'température tunnel de maintien op1260',
+    'T° réservoir résine op1260',
+    '1260_T° Lampe IR 1', '1260_T° Lampe IR 2',
+    '1260_T° Lampe IR 3',
+    'Poids résine piece sortie op1260',
+    'N° Série',
     'COMPTEUR PIECE DANS PINCE op1260'
 ]
 
 DF = pd.DataFrame(np.arange(1, 13).reshape(1, 12),
-             columns=COLUMN_NAMES)
+                  columns=COLUMN_NAMES)
 
+name_cleaner = CleanNames(DF)
+
+def test_clean_one_name():
+    new_name = name_cleaner.clean_one_name('T° tunnel gélification op1260(10st4)')
+    assert new_name == 't_tunnel_gelification_op1260_10st4'
 
 def test_clean_names():
     # re search returns None if one letter not in pattern
     clean_pattern = re.compile("[a-z0-9_]+")
-    cleaned_names = clean_names(DF)
+    cleaned_names = name_cleaner.clean_names(DF)
     for name in cleaned_names:
         assert clean_pattern.fullmatch(name) is not None
+
+def test_matching_dict():
+    assert type(name_cleaner.old_new_match) == dict
+    assert len(name_cleaner.old_new_match) == DF.shape[1]
